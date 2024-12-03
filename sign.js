343879eac68d9a637befad43a2734c89fa85ee5d
nanaishmub <aisha.mujahid.kabir@gmail.com>
2024-12-03 23:30:18 +0100

    log in form

--- sign.js
+++ sign.js
@@ -1,51 +1,51 @@
-        document.addEventListener("DOMContentLoaded", () => {
-            const form = document.getElementById("sign.html");
-            const detail_list = document.getElementById("login");
-        
-            function loadDetails() {
-                const detail = JSON.parse(localStorage.getItem("details") || "[]");
-                detail_list.innerHTML="";
-                tasks.forEach((deatail, index) => {
-        
-                    const detailElement = document.createElement("div");
-                    detailElement.classList.add("detail_item");
-                    detailElement.innerHTML = `
-                        <div>
-                            <h4>${detail.name}</h4>
-                            <p>${detail.email}</p>
-                            <p>${detail.password}</p>
-                            <button class="delete-btn" onclick="deleteTask(${index})">Delete</button>
-                            `;
-                    detail_list.appendChild(detailElement);
-                });
-            }
-        
-            function saveDetails(details){
-                localStorage.setItem("details", JSON.stringify(details));
-            }
-        
-            form.addEventListener("submit", (e) => {
-                e.preventDefault();
-                const name = document.getElementById("name").value;
-                const email = document.getElementById("email").value;
-                const password = document.getElementbyId("password").value;
-        
-                const details = JSON.parse(localStorage.getItem("details") || "[]");
-                details.push({name, email, password});
-                saveDetails(details);
-                laodDetails();
-        
-                form.reset();
-            });
-        
-            window.deleteDetail = function (index) {
-                const details = JSON.parse(localStorage.getItem("details") || "[]");
-                details.splice(index, 1);
-                saveTasks(tasks);
-                loadTasks();
-            }
-        
-            loadTasks();
-        
-        })
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("sign.html");
+    const detail_list = document.getElementById("login");
+
+    function loadDetails() {
+        const details = JSON.parse(localStorage.getItem("details") || "[]");
+        detail_list.innerHTML="";
+        details.forEach((detail, index) => {
+
+            const detailElement = document.createElement("div");
+            detailElement.classList.add("detail_item");
+            detailElement.innerHTML = `
+                <div>
+                    <h4>${detail.name}</h4>
+                    <p>${detail.email}</p>
+                    <p>${detail.password}</p>
+                    <button class="delete-btn" onclick="deleteTask(${index})">Delete</button>
+                    `;
+            detail_list.appendChild(detailElement);
+        });
+    }
+
+    function saveDetails(details){
+        localStorage.setItem("details", JSON.stringify(details));
+    }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementbyId("password").value;
+
+        const details = JSON.parse(localStorage.getItem("details") || "[]");
+        details.push({ name, email, password });
+        saveDetails(details);
+        loadDetails();
+
+        form.reset();
+    });
+
+    window.deleteDetail = function (index) {
+        const details = JSON.parse(localStorage.getItem("details") || "[]");
+        details.splice(index, 1);
+        saveTasks(tasks);
+        loadTasks();
+    }
+
+    loadTasks();
+
+})
         
