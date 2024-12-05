193dcfc2b790bf9ce5be3e2afcfcf56e71af3e0c
nanaishmub <aisha.mujahid.kabir@gmail.com>
2024-12-05 10:52:00 +0100

    final form

--- sign.js
+++ sign.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("sign.html");
-    const detail_list = document.getElementById("login");
+    const form = document.getElementById("login");
+    const detail_list = document.getElementById("info");
 
     function loadDetails() {
         const details = JSON.parse(localStorage.getItem("details") || "[]");
         detail_list.innerHTML="";
         details.forEach((detail, index) => {
-
             const detailElement = document.createElement("div");
             detailElement.classList.add("detail_item");
             detailElement.innerHTML = `
@@ -14,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     <h4>${detail.name}</h4>
                     <p>${detail.email}</p>
                     <p>${detail.password}</p>
-                    <button class="delete-btn" onclick="deleteTask(${index})">Delete</button>
+                    <button class="delete-btn" onclick="deleteDetail(${index})">Delete</button>
                     `;
             detail_list.appendChild(detailElement);
         });
@@ -28,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         const name = document.getElementById("name").value;
         const email = document.getElementById("email").value;
-        const password = document.getElementbyId("password").value;
+        const password = document.getElementById("password").value;
 
         const details = JSON.parse(localStorage.getItem("details") || "[]");
         details.push({ name, email, password });
@@ -41,11 +40,11 @@ document.addEventListener("DOMContentLoaded", () => {
     window.deleteDetail = function (index) {
         const details = JSON.parse(localStorage.getItem("details") || "[]");
         details.splice(index, 1);
-        saveTasks(tasks);
-        loadTasks();
+        saveDetails(details);
+        loadDetails();
     }
 
-    loadTasks();
+    loadDetails();
 
 })
         
